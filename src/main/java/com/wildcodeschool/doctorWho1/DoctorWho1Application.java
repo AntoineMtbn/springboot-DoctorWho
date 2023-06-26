012c9756ca6595d132d90b54e5629fe83059a3da
AntoineMtbn <antoine.montauban@gmail.com>
2023-06-26 17:22:54 +0200

    Initial commit

---
+++ src/main/java/com/wildcodeschool/doctorWho1/DoctorWho1Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorWho1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorWho1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorWho1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/doctorWho1/DoctorWho1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorWho1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorWho1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
